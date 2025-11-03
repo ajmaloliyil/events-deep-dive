df1857d7d7f8cc573af375bb43b181898d79f093
Ajmal O M <amohamed@suyati.com>
2025-11-03 11:42:34 +0530

    text change2 [skip ci]

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -5,7 +5,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h3>{title}3</h3>
+      <h3>{title}4</h3>
       <p>{text}</p>
     </article>
   );
